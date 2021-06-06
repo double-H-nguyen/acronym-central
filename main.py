c278ede687ee2cd8f370635111fa18d2aa4d4004
Henry Nguyen <nguyenhenryh1996@gmail.com>
2021-06-06 15:14:05 -0500

    Allow consecutive searches

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 def main():
-    user_input = input("Type in an acronym: \n")
-    user_input = user_input.lower()
+    while True:
+        user_input = input("Type in an acronym/abbreviation: \n")
+        user_input = user_input.lower()
 
-    if (user_input == "mon"):
-        print("Monday")
-    elif (user_input == "tue"):
-        print("Tuesday")
-    elif (user_input == "wed"):
-        print("Wednesday")
-    elif (user_input == "thurs"):
-        print("Thursday")
-    elif (user_input == "fri"):
-        print("Friday")
-    elif (user_input == "lol"):
-        print("Laugh Out Loud")
-    else:
-        print("That acronym does not exist")
+        if (user_input == "mon"):
+            print("Monday")
+        elif (user_input == "tue"):
+            print("Tuesday")
+        elif (user_input == "wed"):
+            print("Wednesday")
+        elif (user_input == "thurs"):
+            print("Thursday")
+        elif (user_input == "fri"):
+            print("Friday")
+        elif (user_input == "lol"):
+            print("Laugh Out Loud")
+        else:
+            print("That acronym does not exist")
 
+        user_input = input(
+            "Do you want to search another acronym? Type 'no' to exit program or enter anything to continue: ")
+        if (user_input.lower() == 'no'):
+            break
+
+    print('Thanks for using Acronym Central!')
 
 if __name__ == "__main__":
     main()
